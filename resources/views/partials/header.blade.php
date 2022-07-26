b01c7c2b2b0d5e533b64c4220cdbddb92c50717f
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-26 17:20:06 +0200

    bootstrap problem fixed, navbar problem fixed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -45,20 +45,35 @@ $navMenu =
 ?>
 
 
-    <div class="container d-flex justify-content-between align-items-center py-3">
-        <div>
-            <img src={{asset("images/dc-logo.png")}} alt="">
-        </div>
-
-        <nav>
-            <ul class="nav myNav">
-                <li class="nav-item">
-                    @foreach ($navMenu as $menuLink)
-                    <a class="nav-link position-relative" href="{{$menuLink['href']}}">
-                        <h6 class="mb-0 myLink">{{$menuLink['link']}}</h6>
-                    </a>
-                    @endforeach
-                </li>
-            </ul>
-        </nav>
+<div class="container d-flex justify-content-between align-items-center py-3">
+    <div>
+        <img src={{asset("images/dc-logo.png")}} alt="">
     </div>
+
+    {{-- <nav class="navbar">
+        <ul class="navbar-nav myNav">
+            <li class="nav-item">
+                @foreach ($navMenu as $menuLink)
+                <a class="nav-link position-relative" href="{{$menuLink['href']}}">
+                    <h6 class="mb-0 myLink">{{$menuLink['link']}}</h6>
+                </a>
+                @endforeach
+            </li>
+        </ul>
+    </nav> --}}
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav myNav">
+                    @foreach($navMenu as $menuLink)
+                    <li class="nav-item">
+                        <a class="nav-link position-relative" href="{{$menuLink['href']}}">
+                            <h6 class="mb-0 myLink">{{$menuLink['link']}}</h6>
+                        </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
