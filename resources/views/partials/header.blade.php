0cb4f60776f9a232fb44de90a0f4cddcd9096074
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-26 17:38:15 +0200

    header details, little top bar & jumbotron image

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -43,37 +43,37 @@ $navMenu =
                 ],
             ]
 ?>
-
-
-<div class="container d-flex justify-content-between align-items-center py-3">
-    <div>
-        <img src={{asset("images/dc-logo.png")}} alt="">
+<header>
+    <div class="topHeader">
+        <div class="container d-flex justify-content-end align-items-center myGap">
+            <small class="text-white mySmall">DC POWER VISA</small>
+            <small class="text-white mySmall">ADDITIONAL DC SITES</small>
+        </div>
     </div>
 
-    {{-- <nav class="navbar">
-        <ul class="navbar-nav myNav">
-            <li class="nav-item">
-                @foreach ($navMenu as $menuLink)
-                <a class="nav-link position-relative" href="{{$menuLink['href']}}">
-                    <h6 class="mb-0 myLink">{{$menuLink['link']}}</h6>
-                </a>
-                @endforeach
-            </li>
-        </ul>
-    </nav> --}}
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav myNav">
-                    @foreach($navMenu as $menuLink)
-                    <li class="nav-item">
-                        <a class="nav-link position-relative" href="{{$menuLink['href']}}">
-                            <h6 class="mb-0 myLink">{{$menuLink['link']}}</h6>
-                        </a>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
+    <div class="container d-flex justify-content-between align-items-center py-3">
+        <div>
+            <img src={{asset("images/dc-logo.png")}} alt="">
         </div>
-    </nav>
-</div>
+    
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav myNav">
+                        @foreach($navMenu as $menuLink)
+                        <li class="nav-item">
+                            <a class="nav-link position-relative" href="{{$menuLink['href']}}">
+                                <h6 class="mb-0 myLink">{{$menuLink['link']}}</h6>
+                            </a>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div>
+        <img class="myImg" src={{asset("images/jumbotron.jpg")}} alt="">
+    </div>
+</header>
